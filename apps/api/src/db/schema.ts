4c88aa82344ede265880e0a2c51570d6c21dc91b
Jakub Markiewicz <jakub.markiewicz.p@gmail.com>
2025-02-15 15:19:22 +0100

    set up drizzle postgres

---
+++ apps/api/src/db/schema.ts
@@ -0,0 +1,14 @@
+import { index, integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const wordsTable = pgTable(
+  "words",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    word: varchar({ length: 255 }).notNull().unique(),
+    letters: varchar({ length: 35 }).notNull(),
+  },
+  (table) => [
+    index("idx_word").on(table.word),
+    index("idx_letters").on(table.letters),
+  ],
+);
