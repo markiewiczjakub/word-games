a7acdcc978e9b3cfc3cd43e85042ca4fff305af8
Jakub Markiewicz <jakub.markiewicz.p@gmail.com>
2025-02-15 15:38:27 +0100

    add linting

--- apps/api/src/hc.ts
+++ apps/api/src/hc.ts
@@ -1,9 +1,6 @@
 import { app } from "./app";
 import { hc } from "hono/client";
 
-// assign the client to a variable to calculate the type when compiling
-const client = hc<typeof app>("");
-export type Client = typeof client;
-
-export const hcWithType = (...args: Parameters<typeof hc>): Client =>
+export const hcWithType = (...args: Parameters<typeof hc>) =>
   hc<typeof app>(...args);
+export type Client = ReturnType<typeof hcWithType>;
